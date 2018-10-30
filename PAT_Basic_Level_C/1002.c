ed75bf5c981a81978347b99237548d2eeb0baff7
higuys666 <zy163uxiang@163.com>
2018-10-30 23:51:43 +0800

    Update PAT_Basic_Level_C/1002.c

--- PAT_Basic_Level_C/1002.c
+++ PAT_Basic_Level_C/1002.c
@@ -1,5 +1,12 @@
+/*
+1002 写出这个数 （20 分）
+读入一个正整数 n，计算其各位数字之和，用汉语拼音写出和的每一位数字。
+*/
+#include<stdio.h>
 #include<stdlib.h>
-void main()
+int main()
 {
-	print("hello frank!");
+	printf("hello frank!");
+	system("pause");
+	return 0;	
 }
